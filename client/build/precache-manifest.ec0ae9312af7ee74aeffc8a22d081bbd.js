f4cc5e898e4d8f967803f5f26bfbe33284d7a3d1
Sushi Sama <Phongsavanhx@gmail.com>
2019-03-10 16:34:19 -0700

    omgggg hopefully works

---
+++ client/build/precache-manifest.ec0ae9312af7ee74aeffc8a22d081bbd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ffaad475383d25bf1e1",
+    "url": "/static/js/main.2ffaad47.chunk.js"
+  },
+  {
+    "revision": "826400dc847b7387f73d",
+    "url": "/static/js/1.826400dc.chunk.js"
+  },
+  {
+    "revision": "c8aff78ef6c26e609ab09e2d8aa8619c",
+    "url": "/index.html"
+  }
+];
